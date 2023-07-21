8e262e325d898d6d490d334b8c4bb627ff73ef75
Jack Matthews <jm5112356@gmail.com>
2023-07-20 22:19:45 -0400

    ISO format, better settings

--- src/lib/renderTimestamp.ts
+++ src/lib/renderTimestamp.ts
@@ -1,11 +1,14 @@
 import { storage } from "@vendetta/plugin";
+import { Mode } from "../ui/pages/Settings";
 
-export default function renderTimestamp(timestamp: moment.Moment, mode: string = storage.selected): string {
+export default function renderTimestamp(timestamp: moment.Moment, mode: Mode["key"] = storage.selected): string {
     switch (mode) {
         case "calendar":
             return timestamp.calendar();
         case "relative":
             return timestamp.fromNow();
+        case "iso":
+            return timestamp.toISOString();
         case "custom":
             return timestamp.format(storage.customFormat);
     }
--- src/ui/pages/Settings.tsx
+++ src/ui/pages/Settings.tsx
@@ -1,43 +1,51 @@
-import { findByName, findByProps } from "@vendetta/metro";
 import { moment } from "@vendetta/metro/common";
 import { storage } from "@vendetta/plugin";
 import { useProxy } from "@vendetta/storage";
 import { Forms, General } from "@vendetta/ui/components";
 import renderTimestamp from "../../lib/renderTimestamp";
+import { CustomTimeInputRow } from "../components/CustomTimeInputRow";
+import { SelectableRow } from "../components/SelectableRow";
 
 const { ScrollView } = General;
-const { FormSection, FormRow, FormDivider, FormSwitchRow } = Forms;
-const RowCheckmark = findByName("RowCheckmark");
-const { ClearButtonVisibility, default: InputView } = findByProps("ClearButtonVisibility");
+const { FormSection, FormDivider, FormSwitchRow } = Forms;
 
-function SelectableRow({ label, subLabel, selected, onPress }) {
-    return <FormRow label={label} subLabel={subLabel} trailing={<RowCheckmark selected={selected} />} onPress={onPress} />
+export interface Mode {
+    label: string;
+    key: "calendar" | "relative" | "custom" | "iso";
+    renderExtra?: (selected: boolean) => JSX.Element;
 }
 
-function CustomTimeInputRow({ value, onChangeText, placeholder }) {
-    return <InputView
-        value={value}
-        onChangeText={onChangeText}
-        placeholder={placeholder}
-        clearButtonVisibility={ClearButtonVisibility.WITH_CONTENT}
-        showBorder={false}
-        showTopContainer={false}
-        style={{ paddingHorizontal: 15, paddingVertical: 13 }}
-    />
-}
-let modes = [{ label: "Calendar", key: "calendar" }, { label: "Relative", key: "relative" }, { label: "Custom", key: "custom" }]
+let modes: Mode[] = [
+    {
+        label: "Calendar",
+        key: "calendar"
+    },
+    {
+        label: "Relative",
+        key: "relative"
+    },
+    {
+        label: "ISO 8601",
+        key: "iso"
+    },
+    {
+        label: "Custom",
+        key: "custom",
+        renderExtra: (selected) => <CustomTimeInputRow value={storage.customFormat} onChangeText={(t) => storage.customFormat = t} placeholder="dddd, MMMM Do YYYY, h:mm:ss a" disabled={!selected} />
+    }
+]
 
 export default function Settings() {
     useProxy(storage)
 
     return <ScrollView>
-        <FormSection>
-            {modes.map(({ label, key }, i) => <>
+        <FormSection title="Mode">
+            {modes.map(({ label, key, renderExtra }, i) => <>
                 <SelectableRow label={label} subLabel={renderTimestamp(moment(), key)} selected={storage.selected === key} onPress={() => storage.selected = key} />
+                {renderExtra && renderExtra(storage.selected === key)}
                 {i !== modes.length - 1 && <FormDivider />}
             </>
             )}
-            {storage.selected === "custom" && <CustomTimeInputRow value={storage.customFormat} onChangeText={(t) => storage.customFormat = t} placeholder="dddd, MMMM Do YYYY, h:mm:ss a" />}
         </FormSection>
         <FormSection>
             <FormSwitchRow
